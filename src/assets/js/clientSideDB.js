d314888d5adc71075daf668c04b29c33bf7c4793
AttemptedCollective <56233078+AttemptedCollective@users.noreply.github.com>
2020-03-10 14:39:58 +0000

    remove logs

--- src/assets/js/clientSideDB.js
+++ src/assets/js/clientSideDB.js
@@ -129,7 +129,7 @@ async function clientGetStockTotals(formattedData, chosenStock, range) {
 
 //
 async function clientListOfOutgoingDeliveries(isNull) {
-    let response = await fetch('/api/getOutgoingDeliveries?isNull='+isNull[0]+'&today='+today.slice(0,10), getFetchOptions);
+    let response = await fetch('/api/getOutgoingDeliveries?isNull='+isNull+'&today='+today.slice(0,10), getFetchOptions);
     let data = await checkResponse(response);
 
     let tableColumns = [
